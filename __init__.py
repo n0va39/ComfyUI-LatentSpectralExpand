675a8d7625a2ea56b31c62edcbc98423af427251
n0va39 <jadenkwonb1@gmail.com>
2026-05-28 12:38:05 +0900

    Register LSE context planning nodes

--- __init__.py
+++ __init__.py
@@ -1,17 +1,31 @@
-from .latent_spectral_expand import LatentSpectralExpand, SplitSigmaArrayLSE, SplitSigmaArrayDenoiseLSE
+from .latent_spectral_expand import (
+    LatentSpectralExpand,
+    LSESegmentSigmaPlanner,
+    LSEStagePrepare,
+    LSESegmentSigmas2,
+    LSESegmentSigmas3,
+    SplitSigmaArrayLSE,
+    SplitSigmaArrayDenoiseLSE,
+)
 
 NODE_CLASS_MAPPINGS = {
     "LatentSpectralExpand": LatentSpectralExpand,
+    "LSESegmentSigmaPlanner": LSESegmentSigmaPlanner,
+    "LSEStagePrepare": LSEStagePrepare,
+    "LSESegmentSigmas2": LSESegmentSigmas2,
+    "LSESegmentSigmas3": LSESegmentSigmas3,
     "SplitSigmaArrayLSE": SplitSigmaArrayLSE,
-    "SplitSigmaArrayDenoiseLSE": SplitSigmaArrayDenoiseLSE
+    "SplitSigmaArrayDenoiseLSE": SplitSigmaArrayDenoiseLSE,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LatentSpectralExpand": "Latent Spectral Expand (LSE)",
+    "LSESegmentSigmaPlanner": "LSE Segment Sigma Planner",
+    "LSEStagePrepare": "LSE Stage Prepare",
+    "LSESegmentSigmas2": "LSE Segment Sigmas 2",
+    "LSESegmentSigmas3": "LSE Segment Sigmas 3",
     "SplitSigmaArrayLSE": "Split Sigma Array (LSE)",
-    "SplitSigmaArrayDenoiseLSE": "Split Sigma Array Denoise (LSE)"
+    "SplitSigmaArrayDenoiseLSE": "Split Sigma Array Denoise (LSE)",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
-
-
